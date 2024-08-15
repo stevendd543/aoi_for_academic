f92a84503e905f04924e8c442638f57caeaf8915
stevendd543 <72430919+stevendd543@users.noreply.github.com>
2024-08-15 08:52:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def parse_args():
     parser.add_argument('--thr', type=float, default=0.78, help="threshold of classfication.")
     parser.add_argument('--resunet', type=str, default="weights\\resunet.pth", help="weight path of segmentation")
     parser.add_argument('--detector', type=str, default="weights\\detector2.pth", help="weight path of detector")
-    parser.add_argument('--src', type=str, default = "D:\\Users\\THCHENAQ\\Desktop\\bad2", help="source of detection")
-    parser.add_argument('--des', type=str, default = "D:\\Users\\THCHENAQ\\Desktop\\r4", help="saving path of result")
+    parser.add_argument('--src', type=str, help="source of detection")
+    parser.add_argument('--des', type=str, help="saving path of result")
     parser.add_argument('--gpu', type=bool, default=False, help="execution with GPU")
     args = parser.parse_args()
-
+    return args
 if __name__ == "__main__":
     args = parse_args()
     imp = image_process.ImageProcessor(args)
@@ -30,11 +30,9 @@ if __name__ == "__main__":
     elif args.op == 'sg': # segmentation
         imp.segmentation()
     elif args.op == 'det': # detection
-        imp = image_process.ImageProcessor(None)
-        detector = detect.Detector(args)
-        model = detector.create_system()
         resunet = detect.create_resunet()
-        model.eval()
+        detector = detect.Detector(args = args, seg_model = resunet)
+        detector.create_system()
         print("start detecting...")
         for idx,file_name in enumerate(os.listdir(args.src)):
             print(f"{idx}-th detection : {file_name}")
